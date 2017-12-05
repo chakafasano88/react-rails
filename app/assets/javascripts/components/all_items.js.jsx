61b4e60792f2a4432953f72daea98d82d038a0f6
Matt Fasano <matt.fasano88@gmail.com>
2017-12-05 18:10:13 -0500

    rendered JSON object

--- app/assets/javascripts/components/all_items.js.jsx
+++ app/assets/javascripts/components/all_items.js.jsx
@@ -7,7 +7,6 @@ class AllItems extends React.Component {
       };
   }
 
-
   componentDidMount() {
   $.getJSON(
     '/api/v1/items.json', (response) => {
@@ -16,10 +15,18 @@ class AllItems extends React.Component {
   }
 
 render() {
+  const itemsArray = this.state.items.map((key) => {
+    return (
+      <div key={key.id}>
+        <h3>{key.name}</h3>
+        <p>{key.description}</p>
+      </div>
+    )
+  });
    return (
      <div>
        <h1>All items component</h1>
-        <h3></h3>
+        <h3>{itemsArray}</h3>
       </div>
     )
   }
